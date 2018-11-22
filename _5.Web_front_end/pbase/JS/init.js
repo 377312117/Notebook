e94e91fff80ea7becacc588e4ff8750f6eee64de
zzx <377312117@qq.com>
2018-11-22 19:11:36 +0800

    html

--- _5.Web_front_end/pbase/JS/init.js
+++ _5.Web_front_end/pbase/JS/init.js
@@ -1,3 +1,9 @@
+function $2(tagName){
+    // 返回节点数组
+    var elems =  document.getElementsByTagName(tagName);
+    return elems;
+}
+
 function $(tagName){
     // 函数接收字符字符串参数,表示标签名
     // 获取元素节点,不能用标签名直接获取,用get方法获取属性值
